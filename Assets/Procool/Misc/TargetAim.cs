3d54f95fd5e1a45d4a76c3169c3043a7499ae053
SardineFish <sardine@live.cn>
2020-07-30 18:14:02 +0800

    Update lighting.

---
+++ Assets/Procool/Misc/TargetAim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Procool.Misc
+{
+    [ExecuteInEditMode]
+    public class TargetAim : MonoBehaviour
+    {
+        public Transform target;
+        public Vector3 forward = Vector3.forward;
+
+        private void Update()
+        {
+            if (target)
+            {
+                var dir = this.transform.worldToLocalMatrix.MultiplyVector(
+                    (target.transform.position - transform.position)).normalized;
+
+                var rotation = transform.rotation;
+                rotation *= Quaternion.FromToRotation(forward, dir);
+                transform.rotation *= Quaternion.FromToRotation(forward, dir);
+                
+            }
+        }
+    }
+}
