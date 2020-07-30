e835169efe5ce8e379e9fee2dd45649de86be205
SardineFish <sardine@live.cn>
2020-07-30 10:58:58 +0800

    Try 3D buildings.

--- Assets/Procool/Rendering/CityMeshGenerator.Building.cs
+++ Assets/Procool/Rendering/CityMeshGenerator.Building.cs
@@ -12,22 +12,61 @@ namespace Procool.Rendering
         public Mesh BuildingMesh { get; } = new Mesh();
         private List<Vector3> buildingMeshVerts = new List<Vector3>();
         private List<int> buildingMeshTriangles = new List<int>();
+        // private List<Vector3> buildingNormals = new List<Vector3>();
 
         void AddBuilding(Region region)
         {
+            var height = Mathf.Lerp(5, 100, Mathf.Pow(UnityEngine.Random.value, 2));
             var offset = buildingMeshVerts.Count;
             for (var i = 0; i < region.Vertices.Count; i++)
             {
-                var pos = region.Vertices[i].Pos;
+                var pos = region.Vertices[i].Pos.ToVector3(-height);
                 buildingMeshVerts.Add(pos);
             }
-
             for (var i = 2; i < region.Vertices.Count; i++)
             {
                 buildingMeshTriangles.Add(offset);
                 buildingMeshTriangles.Add(offset + i);
                 buildingMeshTriangles.Add(offset + i - 1);
             }
+
+            offset += region.Vertices.Count;
+            foreach (var edge in region.Edges)
+            {
+                var (a, b) = edge.Points;
+                buildingMeshVerts.Add(a.Pos);
+                buildingMeshVerts.Add(b.Pos);
+                buildingMeshVerts.Add(a.Pos.ToVector3(-height));
+                buildingMeshVerts.Add(b.Pos.ToVector3(-height));
+                buildingMeshTriangles.Add(offset + 0);
+                buildingMeshTriangles.Add(offset + 2);
+                buildingMeshTriangles.Add(offset + 1);
+                buildingMeshTriangles.Add(offset + 2);
+                buildingMeshTriangles.Add(offset + 3);
+                buildingMeshTriangles.Add(offset + 1);
+                offset += 4;
+            }
+            // for (var i = 0; i < region.Vertices.Count; i++)
+            // {
+            //     var pos = region.Vertices[i].Pos.ToVector3(-height);
+            //     buildingMeshVerts.Add(pos);
+            // }
+            //
+            // for (var i = 0; i < region.Vertices.Count; i++)
+            // {
+            //     var pos = region.Vertices[i].Pos;
+            //     buildingMeshVerts.Add(pos);
+            // }
+            //
+            // for (var i = 0; i < region.Edges.Count; i++)
+            // {
+            //     buildingMeshTriangles.Add(offset + i + region.Vertices.Count);
+            //     buildingMeshTriangles.Add(offset + i);
+            //     buildingMeshTriangles.Add(offset + (i + 1) % region.Vertices.Count + region.Vertices.Count);
+            //     buildingMeshTriangles.Add(offset + i);
+            //     buildingMeshTriangles.Add(offset + (i + 1) % region.Vertices.Count);
+            //     buildingMeshTriangles.Add(offset + (i + 1) % region.Vertices.Count + region.Vertices.Count);
+            // }
         }
         void GenBuildingMesh(BuildingBlock buildingBlock)
         {
@@ -51,6 +90,7 @@ namespace Procool.Rendering
             BuildingMesh.SetVertices(buildingMeshVerts);
             BuildingMesh.SetTriangles(buildingMeshTriangles, 0);
             BuildingMesh.RecalculateBounds();
+            BuildingMesh.RecalculateNormals();
         }
 
         void GenerateBuildingMesh(City city)
